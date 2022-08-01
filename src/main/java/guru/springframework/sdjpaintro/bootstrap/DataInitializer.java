e501fc44082a5f653c9facbd47c551cc949d33ff
echcherrate.ismail <echcherrate.ismail@gmail.com>
2022-08-01 21:56:22 +0200

    H2 in mem mysql mode, schema initialization.

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,12 +3,14 @@ package guru.springframework.sdjpaintro.bootstrap;
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by jt on 6/12/21.
  */
 @Component
+@Profile({"local", "default"})
 public class DataInitializer implements CommandLineRunner {
 
     private final BookRepository bookRepository;
@@ -19,13 +21,10 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        if(bookRepository.count()>0) bookRepository.deleteAll();
+
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
-
-        System.out.println("Id: " + bookDDD.getId() );
-
         Book savedDDD = bookRepository.save(bookDDD);
-
-        System.out.println("Id: " + savedDDD.getId() );
 
         Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
         Book savedSIA = bookRepository.save(bookSIA);
@@ -34,6 +33,5 @@ public class DataInitializer implements CommandLineRunner {
             System.out.println("Book Id: " + book.getId());
             System.out.println("Book Title: " + book.getTitle());
         });
-
     }
 }
